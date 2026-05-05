1bc98c38f9ec60795b5eaac3a47f09f754ecc0f3
Kike Gil <enrique.gilzara@gmail.com>
2026-05-04 21:59:20 -0600

    Revert "chore: change port to 3019 to avoid EADDRINUSE on VPS"
    This reverts commit 9b22986c82eb7365a57cead82153e53d03016010.

--- cravi/backend/server.js
+++ cravi/backend/server.js
@@ -452,7 +452,7 @@ const server = http.createServer((req, res) => {
   }
 });
 
-const PORT = 3019;
+const PORT = 3000;
 server.listen(PORT, () => {
   console.log(`Server running without dependencies at http://localhost:${PORT}`);
 });
